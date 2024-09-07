7253efe5b88da7ddd6b7070d145d9ae016710be3
Jyrone Parker <jyrone.parker@atlasied.com>
2024-09-07 10:06:21 -0400

    site url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  site: { url: process.env.SITE_NAME },
+  site: { url: 'paid-pwa.jyroneparker.com' },
   ssr: false,
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
