e2e4bacb3dc0239bcc66bbaaca93bc6d22500983
Jyrone Parker <jyrone.parker@atlasied.com>
2024-09-07 10:30:31 -0400

    nuxt update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  site: {
+    url: 'paid-pwa.jyroneparker.com'
+  },
   ssr: false,
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
